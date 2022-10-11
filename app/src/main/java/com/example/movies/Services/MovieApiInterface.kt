9852864ae07b498543218898a0493c36c8b82304
4Level <57768834+KhadidjaHbl@users.noreply.github.com>
2022-10-11 14:22:50 +0100

    Last version

--- app/src/main/java/com/example/movies/Services/MovieApiInterface.kt
+++ app/src/main/java/com/example/movies/Services/MovieApiInterface.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface MovieApiInterface {
-    
+
     @GET("/3/discover/movie?api_key=c9856d0cb57c3f14bf75bdc6c063b8f3")
     fun getMovieList(): Call<MovieResponse>
 
